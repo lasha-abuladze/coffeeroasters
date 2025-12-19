05d362fbfc9fe488d10ab676fd4698c81a1e0b3a
Lasha Abuladze <lashaa.abuladzee@gmail.com>
2025-12-19 14:41:05 +0400

    Style main section; active navigation background needs fix

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const mainSectionHeader = document.querySelector(`.main-section--header`);
 const menuBtn = document.querySelectorAll(`.menu-btn`);
 const navList = document.querySelector(`.nav-list`);
-const li = document.querySelectorAll(`.li`);
+const navListLi = document.querySelectorAll(`.li`);
 
 const navigationBackground = document.querySelector(`.navigation-background`);
 
@@ -16,6 +16,6 @@ mainSectionHeader.addEventListener(`click`, (e) => {
         menuBtn.forEach(el => el.classList.toggle(`display-none`));
         navigationBackground.classList.toggle(`navigation-background--active`)
         navList.classList.toggle(`nav-list--active`);
-        li.forEach(el => el.classList.toggle(`li-active`))
+        navListLi.forEach(el => el.classList.toggle(`li-active`))
     }
 })
